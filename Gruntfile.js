169f3faa72e6d549f1f8c1a6f661c1d4adf446c6
Nico Blum <n.blum@bitexpert.de>
2016-04-28 12:54:33 +0200

    Changed PluginManager from Singleton to normal Class Updated Autoloader to create new instance from PluginManager

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
             mnty: {
                 options: {
                     baseUrl: 'src',
-                    name: 'AutoLoader',
+                    name: 'AutoLoader',git
                     out: 'dist/mnty.js',
                     optimize: 'uglify2',
                     preserveLicenseComments: true,
--- src/AutoLoader.js
+++ src/AutoLoader.js
@@ -5,20 +5,24 @@
  */
 require(['PluginManager', 'OptionParser', 'jquery', 'Window'], function (PluginManager, OptionParser, $, Window) {
 
-    var config = $('script[data-mntyjs]').data('mntyjs');
+    var manager = new PluginManager(),
+        config = $('script[data-mntyjs]').data('mntyjs'),
+        root;
 
     config = OptionParser.parse(config);
 
+    root = config.root ? $(config.root).get(0) : Window.document;
+
     if (config.autoMount !== false) {
         $(Window.document).ready(function () {
-            PluginManager.reconfigure(config);
-            PluginManager.mount(Window.document);
+            manager.reconfigure(config);
+            manager.mount(root);
         });
     }
 
     if (config.autoUnmount !== false) {
         $(Window).on('beforeunload', function () {
-            PluginManager.unmount(Window.document);
+            manager.unmount(root);
         });
     }
 });
